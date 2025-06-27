613a5f1e9bd4630118ce8a67a69935022becc0eb
gelvezz23 <mao_23_@hotmail.com>
2025-06-27 10:27:16 -0500

    [UPD] feat: agregue guardar rutinas

--- src/application/routines/router.ts
+++ src/application/routines/router.ts
@@ -10,7 +10,7 @@ import { verifyToken } from "../../middleware/jwtVerify";
 
 const router = Router();
 
-router.get("/", verifyToken, getRoutines);
+//router.get("/", verifyToken, getRoutines);
 router.post("/ia", verifyToken, generateRoutinesIa);
 router.get("/ia", verifyToken, getGeneratedRoutinesIa);
 router.get("/routinesSaved", verifyToken, getRoutinesSaved);
