1b0ff2b6c52c0226eab7a4e3414cc0d6fa6d76a6
elvisuriel <urielmarciales@gmil.com>
2025-07-25 10:38:43 -0500

    traer rutinas cmpletadas por fecha

--- src/application/routines/router.ts
+++ src/application/routines/router.ts
@@ -1,19 +1,22 @@
 import { Router } from "express";
 import {
-  getRoutines,
+  /* getRoutines, */
   generateRoutinesIa,
   getGeneratedRoutinesIa,
   getRoutinesSaved,
   routinesSaved,
+  getRoutineByDate,
 } from "./controller";
 import { verifyToken } from "../../middleware/jwtVerify";
 
 const router = Router();
 
-router.get("/", verifyToken, getRoutines);
+/* router.get("/", verifyToken, getRoutines); */
 router.post("/ia", verifyToken, generateRoutinesIa);
 router.get("/ia", verifyToken, getGeneratedRoutinesIa);
 router.get("/routinesSaved", verifyToken, getRoutinesSaved);
+router.get("/routinesByDate", verifyToken, getRoutineByDate);
 router.post("/routinesSave", verifyToken, routinesSaved);
+
 
 export default router;
