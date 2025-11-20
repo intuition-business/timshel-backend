874581de255b00780b08d752e7d9c91bfc7dc150
elvisuriel <urielmarciales@gmil.com>
2025-11-20 18:20:22 -0500

    implementar socket y agregar un ejercicio a una rutina

--- src/server/server.ts
+++ src/server/server.ts
@@ -38,6 +38,7 @@ const Server = () => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(helmet());
+  app.use(cors());
   app.use(
     session({
       secret: "EXPRESS_SESSION",
